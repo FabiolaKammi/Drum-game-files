d8028b29a5e1e8c90a3e8c431d0c1a0f74349214
fabiola <fkamil@oliveconsulting.org>
2024-04-25 10:30:33 -0500

    last commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,12 @@
+ 
+
+ document.querySelector(".set").addEventListener("click",function(){
+  var audio = new Audio("sounds/crash.mp3");
+  audio.play();
+ }
+ );
+ document.querySelector(".sets").addEventListener("click",function(){
+  var audio = new Audio("sounds/kick-bass.mp3");
+  audio.play();
+ }
+ );
